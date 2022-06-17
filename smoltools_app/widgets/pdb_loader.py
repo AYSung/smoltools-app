7ea1ebcda26d4f16fae996aa6b85b0f658b24d71
Andrew Sung <aysung300@gmail.com>
2022-06-17 17:46:28 -0500

    feat: better handling of io errors

--- smoltools_app/widgets/pdb_loader.py
+++ smoltools_app/widgets/pdb_loader.py
@@ -3,7 +3,6 @@ import pandas as pd
 import panel as pn
 import panel.widgets as pnw
 
-from utils import colors
 from smoltools.pdbtools import load, select
 from smoltools import fret0
 
@@ -41,26 +40,30 @@ def load_pdb_files(structure_a, chain_id_a, structure_b, chain_id_b) -> pd.DataF
 
 def make_widget(dashboard: Dashboard) -> pn.Column:
     def upload_files(event=None):
-        if pdb_uploader_a.value is None or pdb_uploader_b.value is None:
+        # TODO: better error messages (more specific to conformation)
+        try:
+            structure_a = pdb_uploader_a.value_as_pdb
+            chain_id_a = chain_input_a.value
+            structure_b = pdb_uploader_b.value_as_pdb
+            chain_id_b = chain_input_b.value
+
+            # TODO: allow choosing model number
+            chain_a = select.get_chain(structure_a, model=0, chain=chain_id_a)
+            chain_b = select.get_chain(structure_b, model=0, chain=chain_id_b)
+
+            dashboard.load_pdb_files(chain_a, chain_b)
+            dashboard.load_analyses()
+
+            # TODO: revert to default color (async sleep?)
+            upload_button.button_type = 'success'
+            widget[-1] = ''
+
+        except AttributeError:
             upload_button.button_type = 'warning'
-            return
-
-        structure_a = pdb_uploader_a.value_as_pdb
-        chain_id_a = chain_input_a.value
-        structure_b = pdb_uploader_b.value_as_pdb
-        chain_id_b = chain_input_b.value
-
-        # TODO: allow choosing model number
-        chain_a = select.get_chain(structure_a, model=0, chain=chain_id_a)
-        chain_b = select.get_chain(structure_b, model=0, chain=chain_id_b)
-
-        dashboard.load_pdb_files(chain_a, chain_b)
-        dashboard.load_analyses()
-
-        # TODO: revert to default color (async sleep?)
-        upload_button.button_type = 'success'
-        widget.collapsed = True
-        widget.header_background = colors.GREEN
+            widget[-1] = 'Must select pdb files for both conformations'
+        except KeyError as e:
+            upload_button.button_type = 'warning'
+            widget[-1] = f'Chain {e} not found'
 
     pdb_uploader_a = PDBFileInput(structure_id='conformation_a')
     pdb_uploader_b = PDBFileInput(structure_id='conformation_b')
@@ -70,6 +73,8 @@ def make_widget(dashboard: Dashboard) -> pn.Column:
 
     upload_button = upload_button = pnw.Button(name='Upload', button_type='primary')
     upload_button.on_click(upload_files)
+
+    error_messages = ''
 
     widget = pn.Column(
         pn.Column(
@@ -83,6 +88,7 @@ def make_widget(dashboard: Dashboard) -> pn.Column:
             chain_input_b,
         ),
         pn.Row(upload_button, align='center'),
+        error_messages,
         sizing_mode='stretch_both',
     )
     return widget

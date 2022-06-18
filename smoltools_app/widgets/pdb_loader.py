44ab675f10531d328d85fced279e253a67f40c9a
Andrew Sung <aysung300@gmail.com>
2022-06-17 22:21:09 -0500

    feat: better pdb input widget with error handling

--- smoltools_app/widgets/pdb_loader.py
+++ smoltools_app/widgets/pdb_loader.py
@@ -1,13 +1,26 @@
 import asyncio
 from typing import Protocol
-import pandas as pd
 import panel as pn
 import panel.widgets as pnw
+from panel.viewable import Viewer
+import string
 
 from smoltools.pdbtools import load, select
-from smoltools import fret0
 
 from Bio.PDB.Structure import Structure
+from Bio.PDB.Chain import Chain
+
+
+class NoFileSelected(Exception):
+    def __init__(self, input_id: str):
+        self.input_id = input_id
+
+
+class ChainNotFound(Exception):
+    def __init__(self, input_id, model_id, chain_id):
+        self.input_id = input_id
+        self.model_id = model_id
+        self.chain_id = chain_id
 
 
 class Dashboard(Protocol):
@@ -25,73 +38,88 @@ class PDBFileInput(pnw.FileInput):
 
     @property
     def value_as_pdb(self) -> Structure:
-        return load.read_pdb_from_bytes(self.structure_id, self.value)
+        if self.value is None:
+            raise NoFileSelected(self.structure_id)
+        else:
+            return load.read_pdb_from_bytes(self.structure_id, self.value)
 
 
-def load_pdb_files(structure_a, chain_id_a, structure_b, chain_id_b) -> pd.DataFrame:
-    # TODO: allow choosing model number
-    chain_a = select.get_chain(structure_a, model=0, chain=chain_id_a)
-    chain_b = select.get_chain(structure_b, model=0, chain=chain_id_b)
+class PDBInputWidget(Viewer):
+    def __init__(self, structure_id: str, **params):
+        self.structure_id = structure_id
+        super().__init__(**params)
+        self._pdb_file_input = PDBFileInput(structure_id)
+        self._model_input = pnw.IntInput(name='Model', value=0, width=60)
+        self._chain_input = pnw.Select(
+            name='Chain',
+            options=[char for char in string.ascii_uppercase[:26]],
+            value='A',
+            width=60,
+        )
+        self._layout = pn.Column(
+            f'#### {self.structure_id}:',
+            self._pdb_file_input,
+            pn.Row(
+                self._model_input,
+                self._chain_input,
+            ),
+        )
 
-    distances_a = fret0.chain_to_distances(chain_a)
-    distances_b = fret0.chain_to_distances(chain_b)
+    def __panel__(self):
+        return self._layout
 
-    return fret0.pairwise_distance_between_conformations(distances_a, distances_b)
+    @property
+    def chain(self) -> Chain:
+        structure = self._pdb_file_input.value_as_pdb
+
+        model = self._model_input.value
+        chain_id = self._chain_input.value
+
+        try:
+            return select.get_chain(structure, model, chain_id)
+        except KeyError:
+            raise ChainNotFound(self.structure_id, model, chain_id)
 
 
 def make_widget(dashboard: Dashboard) -> pn.Column:
     async def upload_files(event=None):
-        # TODO: better error messages (more specific to conformation)
+        error_message = ''
         try:
-            # TODO: abstraction layer here
-            structure_a = pdb_uploader_a.value_as_pdb
-            chain_id_a = chain_input_a.value
-            structure_b = pdb_uploader_b.value_as_pdb
-            chain_id_b = chain_input_b.value
-
-            # TODO: allow choosing model number
-            chain_a = select.get_chain(structure_a, model=0, chain=chain_id_a)
-            chain_b = select.get_chain(structure_b, model=0, chain=chain_id_b)
+            chain_a = conformation_a_widget.chain
+            chain_b = conformation_b_widget.chain
 
             dashboard.load_pdb_files(chain_a, chain_b)
             dashboard.load_analyses()
-
+        except ChainNotFound as e:
+            upload_button.button_type = 'warning'
+            error_message += (
+                f'Chain {e.model_id}/{e.chain_id} not found for {e.input_id}\n'
+            )
+        except NoFileSelected as e:
+            upload_button.button_type = 'warning'
+            error_message += f'Must select pdb file for {e.input_id}\n'
+        else:
+            widget[-1] = 'Success!'
             upload_button.button_type = 'success'
-            widget[-1] = ''
-            await asyncio.sleep(2)
+            await asyncio.sleep(3)
             upload_button.button_type = 'primary'
+        finally:
+            widget[-1] = error_message
 
-        except AttributeError:
-            upload_button.button_type = 'warning'
-            widget[-1] = 'Must select pdb files for both conformations'
-        except KeyError as e:
-            upload_button.button_type = 'warning'
-            widget[-1] = f'Chain {e} not found'
+    conformation_a_widget = PDBInputWidget('Conformation A')
+    conformation_b_widget = PDBInputWidget('Conformation B')
 
-    pdb_uploader_a = PDBFileInput(structure_id='conformation_a')
-    pdb_uploader_b = PDBFileInput(structure_id='conformation_b')
-
-    chain_input_a = pnw.TextInput(name='Chain', value='A')
-    chain_input_b = pnw.TextInput(name='Chain', value='A')
-
-    upload_button = upload_button = pnw.Button(name='Upload', button_type='primary')
+    upload_button = upload_button = pnw.Button(
+        name='Upload', button_type='primary', width=150
+    )
     upload_button.on_click(upload_files)
 
-    error_messages = ''
-
     widget = pn.Column(
-        pn.Column(
-            '#### Conformation A:',
-            pdb_uploader_a,
-            chain_input_a,
-        ),
-        pn.Column(
-            '#### Conformation B:',
-            pdb_uploader_b,
-            chain_input_b,
-        ),
+        conformation_a_widget,
+        pn.Spacer(height=10),
+        conformation_b_widget,
+        pn.Spacer(height=30),
         pn.Row(upload_button, align='center'),
-        error_messages,
-        sizing_mode='stretch_both',
+        '',
     )
     return widget

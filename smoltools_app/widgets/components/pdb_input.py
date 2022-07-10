b61420f0059c32513bca6b04b39a52b905f9394e
Andrew Sung <aysung300@gmail.com>
2022-07-10 07:30:15 -0500

    refactor: pdb_loader base class

--- smoltools_app/widgets/components/pdb_input.py
+++ smoltools_app/widgets/components/pdb_input.py
@@ -1,8 +1,11 @@
+from typing import Callable
+
 import panel as pn
 import panel.widgets as pnw
 from panel.viewable import Viewer
 from pathlib import Path
 import string
+import time
 
 from Bio.PDB.Structure import Structure
 from Bio.PDB.Chain import Chain
@@ -62,3 +65,44 @@ class PDBInputWidget(Viewer):
         except KeyError:
             structure_id = Path(self._pdb_file_input.filename).stem
             raise ChainNotFound(structure_id, model, chain_id)
+
+
+class PDBLoader(Viewer):
+    def __init__(self, upload_function: Callable[..., None], **params):
+        super().__init__(**params)
+        self._pdb_input_a = PDBInputWidget('Structure A')
+        self._pdb_input_b = PDBInputWidget('Structure B')
+
+        self._button = pnw.Button(name='Upload', button_type='primary', width=150)
+        self._button.on_click(upload_function)
+
+        self._status = pnw.StaticText()
+
+    def show_error(self, error: Exception) -> None:
+        self._button.button_type = 'warning'
+        self._status.value = f'Error: {error.args[0]}'
+
+    def upload_success(self) -> None:
+        self._status.value = 'Success!'
+        self._button.button_type = 'success'
+        time.sleep(0.5)
+
+    @property
+    def chain_a(self) -> Chain:
+        return self._pdb_input_a.chain
+
+    @property
+    def chain_b(self) -> Chain:
+        return self._pdb_input_b.chain
+
+    def __panel__(self) -> pn.panel:
+        return pn.Card(
+            self._pdb_input_a,
+            pn.Spacer(height=10),
+            self._pdb_input_b,
+            pn.Spacer(height=10),
+            pn.Row(self._button, align='center'),
+            pn.Row(self._status, align='center'),
+            collapsible=False,
+            title='Upload Structures',
+        )
--- smoltools_app/widgets/fret0/pdb_loader.py
+++ smoltools_app/widgets/fret0/pdb_loader.py
@@ -1,44 +1,19 @@
 from typing import Callable
 import panel as pn
-import panel.widgets as pnw
-from panel.viewable import Viewer
-import time
 
-from Bio.PDB.Chain import Chain
-
-from widgets.components.pdb_input import PDBInputWidget
+from widgets.components.pdb_input import PDBLoader
 
 
-class PDBLoader(Viewer):
+class FretPDBLoader(PDBLoader):
     def __init__(self, upload_function: Callable[..., None], **params):
-        super().__init__(**params)
-        self._pdb_input_a = PDBInputWidget('Structure A')
-        self._pdb_input_b = PDBInputWidget('Structure B')
-
-        self._button = pnw.Button(name='Upload', button_type='primary', width=150)
-        self._button.on_click(upload_function)
-
-        self._status = pnw.StaticText()
-
-    def show_error(self, error: Exception) -> None:
-        self._button.button_type = 'warning'
-        self._status.value = f'Error: {error.args[0]}'
-
-    def upload_success(self) -> None:
-        self._status.value = 'Success!'
-        self._button.button_type = 'success'
-        time.sleep(0.5)
-
-    @property
-    def chain_a(self) -> Chain:
-        return self._pdb_input_a.chain
-
-    @property
-    def chain_b(self) -> Chain:
-        return self._pdb_input_b.chain
+        super().__init__(upload_function=upload_function, **params)
 
     @property
     def use_sasa(self) -> bool:
+        raise NotImplementedError
+
+    @property
+    def sasa_cutoff(self) -> float:
         raise NotImplementedError
 
     def __panel__(self) -> pn.panel:

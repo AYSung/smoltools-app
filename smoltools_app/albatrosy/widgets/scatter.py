1a6aa98101ecdf72b923c6a9f5da7d5483762485
Andrew Sung <aysung300@gmail.com>
2022-07-15 12:46:32 -0500

    tweak: resize distance scatter widget

--- smoltools_app/albatrosy/widgets/scatter.py
+++ smoltools_app/albatrosy/widgets/scatter.py
@@ -3,11 +3,16 @@ import panel as pn
 
 from smoltools import albatrosy
 
+from common.widgets.containers import centered_row
+
 
 def make_distance_scatter_widget(data: dict[str, pd.DataFrame]):
+    distance_scatter = pn.pane.Vega(
+        albatrosy.plots.distance_scatter(data['delta'], noe_threshold=15)
+    )
     return pn.Card(
-        pn.pane.Vega(albatrosy.plots.distance_scatter(data['delta'], 15)),
+        centered_row(distance_scatter),
         title='Distance Scatter',
         collapsible=False,
-        width=800,
+        width=900,
     )

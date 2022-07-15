be02601ecda86e97f5df6f0772e62c2c2a317bb2
Andrew Sung <aysung300@gmail.com>
2022-07-15 12:33:49 -0500

    tweaks: dynamic sizing of albatrosy plots

--- smoltools_app/albatrosy/widgets/noe_map.py
+++ smoltools_app/albatrosy/widgets/noe_map.py
@@ -5,6 +5,7 @@ import panel.widgets as pnw
 from smoltools import albatrosy
 
 from common.widgets import table
+from common.widgets.containers import centered_row
 
 
 def make_monomer_noe_widget(data: dict[str, pd.DataFrame]) -> pn.Card:
@@ -15,20 +16,20 @@ def make_monomer_noe_widget(data: dict[str, pd.DataFrame]) -> pn.Card:
         chain_b_id=data['chain_b_id'],
     )
 
-    noe_map_a = albatrosy.plots.noe_map(data['a'])
-    noe_map_b = albatrosy.plots.noe_map(data['b'])
-    combined_noe_map = albatrosy.plots.spliced_noe_map(combined_distances)
+    noe_map_a = pn.pane.Vega(albatrosy.plots.noe_map(data['a']))
+    noe_map_b = pn.pane.Vega(albatrosy.plots.noe_map(data['b']))
+    combined_noe_map = pn.pane.Vega(albatrosy.plots.spliced_noe_map(combined_distances))
 
     return pn.Card(
         pn.Tabs(
-            ('Combined', pn.pane.Vega(combined_noe_map)),
-            ('Conformation A', pn.pane.Vega(noe_map_a)),
-            ('Conformation B', pn.pane.Vega(noe_map_b)),
+            ('Combined', centered_row(combined_noe_map)),
+            ('Conformation A', centered_row(noe_map_a)),
+            ('Conformation B', centered_row(noe_map_b)),
             align='center',
         ),
         title='NOE Maps',
         collapsible=False,
-        width=800,
+        width=900,
     )
 
 
@@ -39,23 +40,29 @@ def make_dimer_noe_widget(data: dict[str, pd.DataFrame]) -> pn.Card:
         chain_a_id=data['chain_a_id'],
         chain_b_id=data['chain_b_id'],
     )
-    intra_chain_noe_map = albatrosy.plots.spliced_noe_map(combined_distances)
-    inter_chain_noe_map = albatrosy.plots.interchain_noe_map(
-        data['delta'], x_title=data['chain_a_id'], y_title=data['chain_b_id']
+    intra_chain_noe_map = pn.pane.Vega(
+        albatrosy.plots.spliced_noe_map(combined_distances)
+    )
+    inter_chain_noe_map = pn.pane.Vega(
+        albatrosy.plots.interchain_noe_map(
+            data['delta'],
+            x_title=data['chain_a_id'],
+            y_title=data['chain_b_id'],
+        )
     )
 
     return pn.Card(
         pn.Tabs(
-            ('Intra-chain NOEs', pn.pane.Vega(intra_chain_noe_map)),
-            ('Inter-chain NOEs', pn.pane.Vega(inter_chain_noe_map)),
-            ('NOE table A', make_noe_table(data['a'])),
-            ('NOE table B', make_noe_table(data['b'])),
-            ('NOE table A-B', make_noe_table(data['delta'])),
+            ('Intra-chain NOEs', centered_row(intra_chain_noe_map)),
+            ('Inter-chain NOEs', centered_row(inter_chain_noe_map)),
+            ('NOE table A', centered_row(make_noe_table(data['a']))),
+            ('NOE table B', centered_row(make_noe_table(data['b']))),
+            ('NOE table A-B', centered_row(make_noe_table(data['delta']))),
             align='center',
         ),
         title='NOE Maps',
         collapsible=False,
-        width=800,
+        width=900,
     )
 
 

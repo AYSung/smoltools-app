ae2e833a1be0cfa522fb9acc8f48f8e3cdeb5b83
Andrew Sung <aysung300@gmail.com>
2022-07-17 15:09:09 -0500

    refactor: factor out analysis logic from app module

--- smoltools_app/albatrosy/app.py
+++ smoltools_app/albatrosy/app.py
@@ -1,91 +1,12 @@
 from typing import Callable
 
-from Bio.PDB.Chain import Chain
 import panel as pn
-import pandas as pd
-from smoltools import albatrosy
 from smoltools.pdbtools.exceptions import ChainNotFound, NoResiduesFound, NoAtomsFound
 
 from utils import colors, config
-from albatrosy.widgets import distance, noe_map, scatter, pdb_loader
+from albatrosy.analysis import run_interchain_analysis, run_conformation_analysis
+from albatrosy.widgets import pdb_loader
 from common.widgets.pdb_loader import NoFileSelected, PDBLoader
-
-
-def run_interchain_analysis(
-    chain_a: Chain, chain_b: Chain, labeled_atoms: dict[str, list[str]]
-) -> list[pn.Card]:
-    data = load_interchain_data(chain_a, chain_b, labeled_atoms)
-    return load_interchain_analyses(data)
-
-
-def _get_chain_id(chain: Chain) -> str:
-    structure_id = chain.get_parent().get_parent().get_id()
-    model_id = chain.get_parent().get_id()
-    chain_id = chain.get_id()
-    return f'{structure_id}/{model_id}/{chain_id}'
-
-
-def load_interchain_data(
-    chain_a: Chain, chain_b: Chain, labeled_atoms: dict[str, list[str]]
-) -> dict[str, pd.DataFrame]:
-    coords_a = albatrosy.coordinates_from_chain(chain_a, labeled_atoms)
-    coords_b = albatrosy.coordinates_from_chain(chain_b, labeled_atoms)
-
-    distances_a = albatrosy.pairwise_distances(coords_a)
-    distances_b = albatrosy.pairwise_distances(coords_b)
-    delta_distances = albatrosy.pairwise_distances(coords_a, coords_b)
-
-    return {
-        'a': distances_a,
-        'b': distances_b,
-        'delta': delta_distances,
-        'chain_a_id': _get_chain_id(chain_a),
-        'chain_b_id': _get_chain_id(chain_b),
-    }
-
-
-def load_interchain_analyses(data: dict[str, pd.DataFrame]) -> list[pn.Card]:
-    return [
-        noe_map.make_dimer_noe_widget(data),
-    ]
-
-
-def run_conformation_analysis(
-    chain_a: Chain, chain_b: Chain, mode: str
-) -> list[pn.Card]:
-    data = load_conformation_data(chain_a, chain_b, mode)
-    return load_conformation_analyses(data)
-
-
-def load_conformation_data(
-    chain_a: Chain, chain_b: Chain, mode: str
-) -> dict[str, pd.DataFrame]:
-    distances_a = albatrosy.coordinates_from_chain(chain_a, mode).pipe(
-        albatrosy.pairwise_distances
-    )
-    distances_b = albatrosy.coordinates_from_chain(chain_b, mode).pipe(
-        albatrosy.pairwise_distances
-    )
-
-    delta_distances = albatrosy.pairwise_distances_between_conformations(
-        distances_a, distances_b
-    )
-
-    return {
-        'a': distances_a,
-        'b': distances_b,
-        'delta': delta_distances,
-        'chain_a_id': _get_chain_id(chain_a),
-        'chain_b_id': _get_chain_id(chain_b),
-    }
-
-
-def load_conformation_analyses(data: dict[str, pd.DataFrame]) -> list[pn.Card]:
-    return [
-        distance.make_distance_widget(data),
-        noe_map.make_monomer_noe_widget(data),
-        scatter.make_distance_scatter_widget(data),
-    ]
 
 
 class Dashboard(pn.template.BootstrapTemplate):

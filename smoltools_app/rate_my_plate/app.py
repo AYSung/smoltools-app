54ecb1b0988786c23906159905ec1a1800fbcc49
Andrew Sung <AYSung@users.noreply.github.com>
2023-03-05 11:25:21 -0600

    Dev (#15)
    * feat: add error handling for if atpase data does not start in A1
    
    * feat: update default value for thresholds
    
    * add example image for atpase data
    
    * feat: add protein concentration input
    
    * feat: add example data to excel_loader

--- smoltools_app/rate_my_plate/app.py
+++ smoltools_app/rate_my_plate/app.py
@@ -1,12 +1,17 @@
 from io import BytesIO
 from pathlib import Path
+import time
 
 import altair as alt
 import panel as pn
 import panel.widgets as pnw
 
 from utils import colors, config
-from rate_my_plate.widgets.excel_loader import ExcelLoader, NoFileSelected
+from rate_my_plate.widgets.excel_loader import (
+    ExcelLoader,
+    NoFileSelected,
+    BadFileFormat,
+)
 from smoltools.rate_my_plate import read_data_from_bytes, rate_plate, convert_to_wide
 from smoltools.rate_my_plate import consumption_curve, kinetics_curves
 from rate_my_plate.widgets.excel_download import excel_file_download
@@ -28,13 +33,18 @@ class Dashboard(pn.template.BootstrapTemplate):
             name='lower percent cutoff', value=-0.05, step=0.05, start=-0.2, end=0.5
         )
         self._upper_percent = pnw.FloatInput(
-            name='upper percent cutoff', value=0.75, step=0.05, start=0.5, end=1
+            name='upper percent cutoff', value=0.8, step=0.05, start=0.5, end=1
+        )
+        self._protein_concentration = pnw.FloatInput(
+            name='protein concentration (uM)', value=1.0
         )
         self._redo_filter_button = pnw.Button(name='Set new thresholds')
         self._redo_filter_button.on_click(self.preview_data)
 
         self._continue_button = pnw.Button(name='Continue', button_type='success')
         self._continue_button.on_click(self.analyze_data)
+
+        self._analysis_status = pnw.StaticText()
 
         self.main.append(
             pn.FlexBox(
@@ -52,6 +62,8 @@ class Dashboard(pn.template.BootstrapTemplate):
             self.data = read_data_from_bytes(byte_file)
         except (ValueError, TypeError, AttributeError):
             self.excel_loader.show_error(NoFileSelected())
+        except KeyError:
+            self.excel_loader.show_error(BadFileFormat())
         else:
             self.excel_loader.upload_success()
             self.preview_data()
@@ -75,32 +87,54 @@ class Dashboard(pn.template.BootstrapTemplate):
                         align='center',
                     ),
                     self._redo_filter_button,
+                    pn.Row(
+                        self._protein_concentration,
+                    ),
                     pn.layout.Divider(),
                     self._continue_button,
+                    self._analysis_status,
                 ),
                 justify_content='center',
             )
         ]
 
     def analyze_data(self, event=None) -> None:
-        self.analyzed_data = rate_plate(
-            self.data,
-            lower_percent=self._lower_percent.value,
-            upper_percent=self._upper_percent.value,
-        )
-        filename = Path(self.excel_loader.input_filename).stem
-        self.main[0].objects = [
-            pn.FlexBox(
-                pn.Column(
-                    pn.pane.Vega(kinetics_curves(self.analyzed_data)),
-                    excel_file_download(
-                        self._download_callback,
-                        f'{filename}-rated.xlsx',
+        try:
+            if self._protein_concentration.value <= 0:
+                raise ValueError
+
+            self.analyzed_data = rate_plate(
+                self.data,
+                lower_percent=self._lower_percent.value,
+                upper_percent=self._upper_percent.value,
+                concentration=self._protein_concentration.value,
+            )
+        except ValueError:
+            self.show_concentration_error()
+        else:
+            self.analysis_success()
+            filename = Path(self.excel_loader.input_filename).stem
+            self.main[0].objects = [
+                pn.FlexBox(
+                    pn.Column(
+                        pn.pane.Vega(kinetics_curves(self.analyzed_data)),
+                        excel_file_download(
+                            self._download_callback,
+                            f'{filename}-rated.xlsx',
+                        ),
                     ),
+                    justify_content='center',
                 ),
-                justify_content='center',
-            ),
-        ]
+            ]
+
+    def show_concentration_error(self) -> None:
+        self._continue_button.button_type = 'warning'
+        self._analysis_status.value = 'Protein concentration must be > 0 uM'
+
+    def analysis_success(self) -> None:
+        self._analysis_status.value = 'Success!'
+        self._continue_button.button_type = 'success'
+        time.sleep(0.5)
 
     def _download_callback(self) -> BytesIO:
         bytes_io = BytesIO()
--- smoltools_app/rate_my_plate/widgets/excel_loader.py
+++ smoltools_app/rate_my_plate/widgets/excel_loader.py
@@ -12,6 +12,12 @@ class NoFileSelected(Exception):
         super().__init__(message)
 
 
+class BadFileFormat(Exception):
+    def __init__(self):
+        message = 'Please double check the table starts in the A1 cell as shown in the example.'
+        super().__init__(message)
+
+
 def excel_file_input() -> pnw.FileInput:
     return pnw.FileInput(accept='.xls,.xlsx')
 
@@ -26,6 +32,7 @@ class ExcelLoader(Viewer):
 
         self._button = pnw.Button(name='Upload', button_type='primary', width=150)
         self._status = pnw.StaticText()
+        self._example = pn.pane.PNG('assets/screenshots/atpase_example.png')
 
     def bind_button(self, function: Callable[..., None]) -> None:
         self._button.on_click(function)
@@ -52,6 +59,8 @@ class ExcelLoader(Viewer):
             self._input_widget,
             pn.Row(self._button, align='center'),
             pn.Row(self._status, align='center'),
+            'Example data format:',
+            pn.Row(self._example, align='center'),
             collapsible=False,
             title='Upload Plate Reader Data',
         )
